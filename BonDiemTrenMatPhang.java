2bf3d05d9fe36fc19b93e2ba1f1643e4fbb2044b
ducanhnguyen07 <113274074+ducanhnguyen07@users.noreply.github.com>
2023-09-10 22:16:42 +0700

    Create BonDiemTrenMatPhang.java

---
+++ BonDiemTrenMatPhang.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package codeptit;
+
+import java.util.*;
+
+/**
+ *
+ * @author Administrator
+ */
+class Point3D{
+    public int x;
+    public int y;
+    public int z;
+
+    public Point3D(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+    public static boolean check(Point3D a, Point3D b, Point3D c, Point3D d){
+        Point3D v1 = new Point3D(a.x-b.x, a.y-b.y, a.z-b.z);
+        Point3D v2 = new Point3D(a.x-c.x, a.y-c.y, a.z-c.z);
+        Point3D n = new Point3D(v1.y*v2.z-v2.y*v1.z, v2.x*v1.z-v1.x*v2.z, v1.x*v2.y-v2.x*v1.y);
+        if(n.x*(d.x - a.x) + n.y*(d.y - a.y) + n.z*(d.z - a.z) == 0)
+            return true;
+        return false;
+    }
+}
+
+public class BonDiemTrenMatPhang {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            Point3D p1 = new Point3D(sc.nextInt(),sc.nextInt(),sc.nextInt());
+            Point3D p2 = new Point3D(sc.nextInt(),sc.nextInt(),sc.nextInt());
+            Point3D p3 = new Point3D(sc.nextInt(),sc.nextInt(),sc.nextInt());
+            Point3D p4 = new Point3D(sc.nextInt(),sc.nextInt(),sc.nextInt());
+
+            if(Point3D.check(p1,p2,p3,p4)){
+                System.out.println("YES");
+            } else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
